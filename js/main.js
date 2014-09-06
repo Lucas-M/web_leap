0dce2830dc8bc092614ced43211d5bf82b9efe72
Lucas-M <lucas.macd@gmail.com>
2014-09-06 09:52:25 -0400

    Canvas with webgl up and running

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+    var gl;
 
+    function draw(canvas) {
+        console.log("CANVAS: " + canvas);
+        var canvas = document.getElementById(canvas);
+        gl = initWebGl(canvas);
+
+        if (gl) {
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            gl.enable(gl.DEPTH_TEST);
+            gl.depthFunc(gl.LEQUAL);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        }
+    }
+
+    function initWebGl(canvas) {
+        gl = null;
+
+        try {
+            // Try to grab the standard context. If it fails, fallback to experimental.
+            gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        }
+        catch(e) {}
+
+        // If we don't have a GL context, give up now
+        if (!gl) {
+            alert("Unable to initialize WebGL. Your browser may not support it.");
+            gl = null;
+        }
+        return gl;
+    }
