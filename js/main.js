0828ef2091ba030b68fdc08f87e08bd18e904945
Lucas-M <lucas.macd@gmail.com>
2015-01-06 20:54:14 -0500

    update index

--- js/main.js
+++ js/main.js
@@ -1,31 +1,117 @@
-    var gl;
+var gl;
 
-    function draw(canvas) {
-        console.log("CANVAS: " + canvas);
-        var canvas = document.getElementById(canvas);
-        gl = initWebGl(canvas);
+function draw(canvas) {
+    console.log("DRAW\n\nCANVAS: " + canvas);
+    var canvas = document.getElementById(canvas);
+    gl = initWebGl(canvas);
 
-        if (gl) {
-            gl.clearColor(0.0, 0.0, 0.0, 1.0);
-            gl.enable(gl.DEPTH_TEST);
-            gl.depthFunc(gl.LEQUAL);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        }
+    if (gl) {
+    	console.log("GL INITED SUCCESSFULLY");
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     }
+}
 
-    function initWebGl(canvas) {
+function initWebGl(canvas) {
+	console.log("INIT WEBGL");
+    gl = null;
+
+    try {
+        // Try to grab the standard context. If it fails, fallback to experimental.
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    }
+    catch(e) {}
+
+    // If we don't have a GL context, give up now
+    if (!gl) {
+        alert("Unable to initialize WebGL. Your browser may not support it.");
         gl = null;
-
-        try {
-            // Try to grab the standard context. If it fails, fallback to experimental.
-            gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-        }
-        catch(e) {}
-
-        // If we don't have a GL context, give up now
-        if (!gl) {
-            alert("Unable to initialize WebGL. Your browser may not support it.");
-            gl = null;
-        }
-        return gl;
     }
+    return gl;
+}
+
+function initShaders() {
+	console.log("INIT SHADERS");
+	var fragmentShader = getShader(gl, "shader-fs");
+	var vertexShader = getShader(gl, "shader-vs");
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	// Alert on fail
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		alert("Can't init shader program.");
+	}
+
+	gl.useProgram(shaderProgram);
+
+	vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(vertexPositionAttribute);
+}
+
+function getShader(gl, id) {
+	console.log("GET SHADER");
+	var shaderScript, theSource, currentChild, shader;
+
+	shaderScript = document.getElementById(id);
+
+	if (!shaderScript) {
+		return null;
+	}
+
+	theSource = "";
+	currentChild = shaderScript.firstChild;
+
+	while(currentChild) {
+		if (currentChild.nodeType == currentChild.TEXT_NODE) {
+			theSource += currentChild.textContent;
+		}
+		currentChild = currentChild.nextSibling;
+	}
+
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shader.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		// What kind of shader is it?
+		return null;
+	}
+
+	gl.shaderSource(shader, theSource);
+
+	// Compile the shader program
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert("Error occurred during compiling of the shaders: " + gl.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+var horizAspect = 480.0/640.0;
+
+function initBuffers() {
+	squareVerticesBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+	var vertices = [
+		1.0, 1.0, 0.0,
+		-1.0, 1.0, 0.0,
+		1.0, -1.0, 0.0,
+		-1.0, -1.0, 0.0
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+}
+
+function drawScene() {
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
